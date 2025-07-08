c6620ab685f75b3c5304fd0d868ed3dfb0f59d91
DiegoRdrz <diegordrz455@gmail.com>
2025-07-08 01:08:39 -0500

    Updates

---
+++ precache-manifest.248380d248a44ae825f3a0bf19f4f950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bbbad10d05ccfa2c9e4c0adb9d28be",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "9d14c2d71cc18faa07f4",
+    "url": "/Portafolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4ce0d659b895d5d9ca24",
+    "url": "/Portafolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "9d14c2d71cc18faa07f4",
+    "url": "/Portafolio/static/js/2.fd43457b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portafolio/static/js/2.fd43457b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce0d659b895d5d9ca24",
+    "url": "/Portafolio/static/js/main.30d9e854.chunk.js"
+  },
+  {
+    "revision": "eb2f6e25ba7bbce609dc",
+    "url": "/Portafolio/static/js/runtime-main.27cfaddd.js"
+  }
+]);
